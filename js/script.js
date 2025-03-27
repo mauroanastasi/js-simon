2a98b99fdd1f17662da29a9875a35e4f2436966b
mauroanastasi <mauroanastasi1@gmail.com>
2025-03-27 15:05:42 +0100

    [UPD] imposto l'esercizio

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Far visualizzare 5 numeri casuali
+// Appena questi numeri vengono visualizzati, far partire un timer di 30 secondi
+// Allo scadere dei 30 secondi, i 5 numeri scompaiono e appare imput dove inserire i numeri visualizzati precedentemente
+// Una volta inseriti il software dice quanti e quali dei numeri da indovinare sono stati individuati
+
+
