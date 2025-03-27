079b13f924bb9f9ab76fe9660749bf9587aa6954
mauroanastasi <mauroanastasi1@gmail.com>
2025-03-27 16:09:44 +0100

    [UPD] genero num

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 // Far visualizzare 5 numeri casuali
+    //  -genero i 5 numeri con for, MathFloor e Random
+    //  -li visualizzo a pagina a pagina
 // Appena questi numeri vengono visualizzati, far partire un timer di 30 secondi
 // Allo scadere dei 30 secondi, i 5 numeri scompaiono e appare imput dove inserire i numeri visualizzati precedentemente
 // Una volta inseriti il software dice quanti e quali dei numeri da indovinare sono stati individuati
+
+// RECUPERO ELEMENTI DOM E INIZIALIZZAZIONE VARIABILI
+//// dove verranno visuallizati i numeri
+const numberslist = document.getElementById(`number-list`)
+//// array dei numeri che compariranno
+let num =[];
+
+// per generare i 5 numeri casuali
+for(i=0;i<5;i++){
+    generoNum = Math.floor(Math.random()*100);
+    num = generoNum
+    
+    numberslist.innerHTML= num
+}
+
+
+
+
+
+
+
+
 
 
