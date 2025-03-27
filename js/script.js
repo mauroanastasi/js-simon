3a558f602ec3dd18bdb4da1fd0a184057aa3303e
mauroanastasi <mauroanastasi1@gmail.com>
2025-03-27 17:17:54 +0100

    [UPD] faccio scomparire anche il coutdown al termine dei 30 sec

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 // Far visualizzare 5 numeri casuali .FATTO.
     //  -genero i 5 numeri con for, MathFloor e Random
     //  -li visualizzo a pagina a pagina 
-// Appena questi numeri vengono visualizzati, far partire un timer di 30 secondi .FATO.
+// Appena questi numeri vengono visualizzati, far partire un timer di 30 secondi .FATTO.
+    // creo funzione che indica cosa faccio in base ai secondi
 // Allo scadere dei 30 secondi, i 5 numeri scompaiono e appare imput dove inserire i numeri visualizzati precedentemente
+
 // Una volta inseriti il software dice quanti e quali dei numeri da indovinare sono stati individuati
 
 /////////// RECUPERO ELEMENTI DOM E INIZIALIZZAZIONE VARIABILI
@@ -39,6 +41,7 @@ const interval = setInterval(function(){
         clearInterval(interval);
         // faccio scomparire i numeri alla fine del countdown
         numberslist.className = numberslist.classList+" d-none"
+        countdown.className = countdown.classList+" d-none"
     }
 },1000)
 
